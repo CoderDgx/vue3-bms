395f38ea9ecf0ed9ed8bc2aa518a42f31cc2655a
CoderDgx <1181571719@qq.com>
2022-03-01 19:37:49 +0800

    feat: 集成第三库,封装axios

---
+++ src/service/request/index.ts
@@ -0,0 +1,109 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosResponse, AxiosInstance, AxiosRequestConfig } from "axios";
+import { ElLoading } from "element-plus";
+
+interface InterceptorHooks {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig;
+  requestInterceptorCatch?: (error: any) => any;
+
+  responseInterceptor?: (response: AxiosResponse) => AxiosResponse;
+  responseInterceptorCatch?: (error: any) => any;
+}
+
+interface RequestConfig extends AxiosRequestConfig {
+  showLoading?: boolean;
+  interceptorHooks?: InterceptorHooks;
+}
+
+interface Data<T> {
+  data: T;
+  returnCode: string;
+  success: boolean;
+}
+
+class AxiosRequest {
+  config: AxiosRequestConfig;
+  interceptorHooks?: InterceptorHooks;
+  showLoading: boolean;
+  loading?: any;
+  instance: AxiosInstance;
+
+  constructor(options: RequestConfig) {
+    this.config = options;
+    this.interceptorHooks = options.interceptorHooks;
+    this.showLoading = options.showLoading ?? true;
+    this.instance = axios.create(options);
+
+    this.setupInterceptor();
+  }
+
+  setupInterceptor(): void {
+    this.instance.interceptors.request.use(
+      this.interceptorHooks?.requestInterceptor,
+      this.interceptorHooks?.requestInterceptorCatch
+    );
+    this.instance.interceptors.response.use(
+      this.interceptorHooks?.responseInterceptor,
+      this.interceptorHooks?.requestInterceptorCatch
+    );
+
+    this.instance.interceptors.request.use((config) => {
+      if (this.showLoading) {
+        this.loading = ElLoading.service({
+          lock: true,
+          text: "Loading",
+          spinner: "el-icon-loading",
+          background: "rgba(0, 0, 0, 0.7)",
+        });
+      }
+      return config;
+    });
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        this.loading?.close();
+        return res;
+      },
+      (err) => {
+        this.loading?.close();
+        return err;
+      }
+    );
+  }
+
+  request<T = any>(config: RequestConfig): Promise<T> {
+    if (!config.showLoading) {
+      this.showLoading = false;
+    }
+    return new Promise((resolve, reject) => {
+      this.instance
+        .request<any, Data<T>>(config)
+        .then((res) => {
+          resolve(res.data);
+          this.showLoading = true;
+        })
+        .catch((err) => {
+          reject(err);
+          this.showLoading = true;
+        });
+    });
+  }
+
+  get<T = any>(config: RequestConfig): Promise<T> {
+    return this.request({ ...config, method: "GET" });
+  }
+
+  post<T = any>(config: RequestConfig): Promise<T> {
+    return this.request({ ...config, method: "POST" });
+  }
+
+  delete<T = any>(config: RequestConfig): Promise<T> {
+    return this.request({ ...config, method: "DELETE" });
+  }
+
+  patch<T = any>(config: RequestConfig): Promise<T> {
+    return this.request({ ...config, method: "PATCH" });
+  }
+}
+
+export default AxiosRequest;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "./",
+  outputDir: "./build",
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", path.resolve(__dirname, "src"))
+      .set("views", "@/views");
+  },
 });
