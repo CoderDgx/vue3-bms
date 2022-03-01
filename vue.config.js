815419939898fc01886a7d04859f009ee2c78619
CoderDgx <1181571719@qq.com>
2022-03-01 19:39:02 +0800

    Revert "feat: 集成第三库,封装axios"
    This reverts commit 395f38ea9ecf0ed9ed8bc2aa518a42f31cc2655a.

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-const path = require("path");
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "./",
-  outputDir: "./build",
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set("@", path.resolve(__dirname, "src"))
-      .set("views", "@/views");
-  },
 });
